033ff17e6b2032775842d2c87978a369f1963b88
Shayne Meyer <shayne.meyer@gmail.com>
2017-07-07 07:38:00 -0700

    added second die and logic

--- app.js
+++ app.js
@@ -8,36 +8,28 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, gamePlaying, previousRoll, gameThreshold;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   if(gamePlaying){
     // 1. random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-    console.log(previousRoll);
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
 
     // 3. Update the round score if the rolled number was not 1.
-    if (previousRoll === 6 && dice === 6) {
-      scores[activePlayer] = 0;
-      nextPlayer();
-    } else if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add score
-      roundScore += dice;
-      previousRoll = dice;
+      roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       nextPlayer();
@@ -53,10 +45,18 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    var input = document.querySelector('.final-score').value;
+
+    // Undefined, 0, null or "" are coerced to false
+    var winningScore = input ? input : 100;
+
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-      document.querySelector('.dice').style.display = 'none';
+
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
+
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -75,12 +75,11 @@ function nextPlayer() {
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
 
-  // document.querySelector('player-0-panel').classList.remove('active');
-  // document.querySelector('player-1-panel').classList.add('active');
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -89,33 +88,24 @@ function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
-  previousRoll = 0;
   gamePlaying = true;
-  gameThreshold = 100;
+
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
+
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
+
   document.getElementById('name-0').textContent = 'Player 1';
   document.getElementById('name-1').textContent = 'Player 2';
+
   document.querySelector('.player-0-panel').classList.remove('winner');
   document.querySelector('.player-1-panel').classList.remove('winner');
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
+
   document.querySelector('.player-0-panel').classList.add('active');
 }
-
-/*
-Change the game rules:
-
-1. A player looses his entire score when he rolls two 6's in a row.
-After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input filed so players can set the winning score. Default is 100.
-(Hint: you can read value with the .value property in JavaScript)
-3. Add another dice to the game, so that there are two dice now. The player looses his current score
-when one of the dice is a 1.
-(Hint: you will need CSS to position the second dice, so take a look at the css for the first dice.)
-
-
- */
--- readme.md
+++ readme.md
@@ -2,8 +2,9 @@
 A simple dice game written in JavaScript and CSS. You can read more about the game [here](https://en.wikipedia.org/wiki/Pig_(dice_game)).
 
 ## Rules
-1. Play starts with player one clicking "ROLL DICE".
-1. If player 1 doesn't roll a 1 or 6 followed by another six, they continue to roll until they click "HOLD" or roll a 1 or consecutive 6's.
-1. Player can keep their current score if they hold. But if they roll a one they lose their round and it is the next players turn.
-1. Game continues until the threshold is met, defaults to 100.
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
 
